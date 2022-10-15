7ba7431d22511f3e73744c9f78b6d7986b14e7e6
Paulo Ferreira <phgferreira@outlook.com>
2022-10-15 17:10:59 -0300

    Implantado uma regra de promoção de funcionário

--- src/main/java/br/com/alura/rh/model/Cargo.java
+++ src/main/java/br/com/alura/rh/model/Cargo.java
@@ -1,10 +1,34 @@
 package br.com.alura.rh.model;
 
+import br.com.alura.rh.ValidacaoException;
+
 public enum Cargo {
 
-	ASSISTENTE,
-	ANALISTA,
-	ESPECIALISTA,
-	GERENTE;
+	ASSISTENTE {
+		@Override
+		public Cargo getProximoCargo() {
+			return Cargo.ANALISTA;
+		}
+	},
+	ANALISTA{
+		@Override
+		public Cargo getProximoCargo() {
+			return Cargo.ESPECIALISTA;
+		}
+	},
+	ESPECIALISTA{
+		@Override
+		public Cargo getProximoCargo() {
+			return Cargo.GERENTE;
+		}
+	},
+	GERENTE{
+		@Override
+		public Cargo getProximoCargo() {
+			return Cargo.GERENTE;
+		}
+	};
+
+	public abstract Cargo getProximoCargo();
 
 }
