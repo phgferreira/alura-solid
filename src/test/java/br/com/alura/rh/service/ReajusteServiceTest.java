36580c847c92e3f82f49a45aa66070201b3fed24
Paulo Ferreira <phgferreira@outlook.com>
2022-10-15 16:57:58 -0300

    Removido imports do Mockito inline que não foram utilizados

--- src/test/java/br/com/alura/rh/service/ReajusteServiceTest.java
+++ src/test/java/br/com/alura/rh/service/ReajusteServiceTest.java
@@ -6,8 +6,6 @@ import br.com.alura.rh.model.Funcionario;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
