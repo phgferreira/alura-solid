9e45b8a6538957659f395d0c2f785bdf628b651b
Paulo Ferreira <phgferreira@outlook.com>
2022-10-15 10:52:46 -0300

    Removido o 'arrenda para cima' na hora de calcular a porcentagem do aumento porque estava permitindo aumento de alguns reais e centavos a mais dos 40%

--- src/main/java/br/com/alura/rh/service/ReajusteService.java
+++ src/main/java/br/com/alura/rh/service/ReajusteService.java
@@ -11,7 +11,7 @@ public class ReajusteService {
 
     public void reajusteSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento) {
         BigDecimal salarioAtual = funcionario.getSalario();
-        BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
+        BigDecimal percentualReajuste = aumento.divide(salarioAtual);
         if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
             throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
         }
