bc5f7ffb247bd582621cd45a11e0f208aecdb0af
Paulo Ferreira <phgferreira@outlook.com>
2022-10-15 09:59:43 -0300

    Adicionado o Maven Framework e adicionado a dependência do JUnit para executar os testes

---
+++ src/test/java/br/com/alura/rh/service/ReajusteServiceTest.java
@@ -0,0 +1,13 @@
+package br.com.alura.rh.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReajusteServiceTest {
+
+    @Test
+    void reajusteSalarioDoFuncionario() {
+
+    }
+}
